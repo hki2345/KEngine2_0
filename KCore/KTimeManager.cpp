a41f97f049ae6c92633d1d2596fddfdb7f001434
KS H <43468069+hki2345@users.noreply.github.com>
2019-08-20 08:03:25 +0900

    TimeManager 완ㅅ어?
    나중 보완 하든가ㅋㅋ

--- KCore/KTimeManager.cpp
+++ KCore/KTimeManager.cpp
@@ -2,6 +2,53 @@
 
 
 
+KTimeManager::KTimer::KTimer()
+{
+
+}
+KTimeManager::KTimer::~KTimer()
+{
+
+}
+
+
+
+void KTimeManager::KTimer::start()
+{
+	QueryPerformanceFrequency(&CpuFrequency);
+
+	// 누적타임 
+	QueryPerformanceCounter(&CurCount);
+	Accumule_Time = 0;
+	FPSCount = 0;
+}
+
+void KTimeManager::KTimer::accumulate()
+{
+	// 델타타임을 구한다.
+	QueryPerformanceCounter(&NextCount);
+
+	DeltaTime = ((float)(NextCount.QuadPart - CurCount.QuadPart)) / ((float)CpuFrequency.QuadPart);
+	CurCount.QuadPart = NextCount.QuadPart;
+
+
+	// FPS 계산 1초당 프레임 수...
+	Accumule_Time += DeltaTime;
+
+	++FPSCount;
+
+	if (1.0f <= Accumule_Time)
+	{
+		Fps = ((float)FPSCount / Accumule_Time);
+		Accumule_Time = 0.0f;
+		FPSCount = 0;
+	}
+}
+
+
+
+
+
 void KTimeManager::init()
 {
 
--- KCore/KTimeManager.h
+++ KCore/KTimeManager.h
@@ -1,4 +1,11 @@
 #pragma once
+#include <Windows.h>
+#include <map>
+#include "KProgress.h"
+#include "KName.h"
+
+
+
 class KTimeManager
 {
 private:
@@ -9,16 +16,47 @@ private:
 	~KTimeManager() = delete;
 
 private:
-	class Timer
+	class KTimer : public KName
 	{
 	public:
-		Timer() {};
-		~Timer() {};
+		friend class KTimeManager;
 
 	private:
+		KTimer();
+		KTimer(const KTimer& _Core) = delete;
+		KTimer(const KTimer&& _Core) = delete;
+		void operator=(const KTimer& _Core) = delete;
+		~KTimer();
 
+	private:
+		void start();
+		void accumulate();
+		void pause();
+
+	private:
+		LARGE_INTEGER CpuFrequency;
+		LARGE_INTEGER CurCount;
+		LARGE_INTEGER NextCount;
+
+		float DeltaTime;
+		float Accumule_Time;
+		float Fps;
+		int FPSCount;
+
+	public:
+		float& fps()
+		{
+			return Fps;
+		}
+
+		float& deltatime()
+		{
+			return DeltaTime;
+		}
 	};
 
+private:
+	std::map<std::wstring, KTimer*> mTimer;
 
 private:
 	static void init();
